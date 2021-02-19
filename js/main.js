1b3d653f442cf331f673104079f54947883d915f
vgupta117 <79120583+vgupta117@users.noreply.github.com>
2021-02-19 10:34:58 -0500

    Layout and style changes
    Layout and style changes to improve readability and legibility of the webpage

--- js/main.js
+++ js/main.js
@@ -89,19 +89,19 @@ $(".eachCard").click(function(){
             break;
         case 'show':
             newTitle = 'TV-SHOW';
-            newDescription = "Gotham is an American action crime drama television series developed by Bruno Heller, produced by Warner Bros. Television and based on characters published by DC Comics and appearing in the Batman franchise, primarily those of James Gordon and Bruce Wayne. The series premiered on Fox on September 20, 2013, and concluded on April 25, 2019. The series although initially intended for Gordon and the city,  included the Bruce Wayne character and the origin stories of Batman's rogues gallery. Development for a prequel series based on Batman began in September 2012, with Bruno Heller hired by Fox to serve as a writer and executive producer. In March 2014, Fox originally ordered 16 episodes for its first season before expanding it to 22. ";
+            newDescription = "Gotham is an American action crime drama television series developed by Bruno Heller, produced by Warner Bros. Television and based on characters published by DC Comics and appearing in the Batman franchise, primarily those of James Gordon and Bruce Wayne. The series premiered on Fox on September 20, 2013, and concluded on April 25, 2019. The series although initially intended for Gordon and the city,  included the Bruce Wayne character and the origin stories of Batman's rogues gallery. Development for a prequel series based on Batman began in September 2012, with Bruno Heller hired by Fox to serve as a writer and executive producer. In March 2014, Fox originally ordered 16 episodes for its first season before expanding it to 22.";
             break;
-        case 'culture':
-            newTitle = 'Culture';
-            newDescription = "Batman writer and editor Dennis O'Neil has said that, figuratively, Batman's Gotham City is akin to 'Manhattan below Fourteenth Street at eleven minutes past midnight on the coldest night in November.' Batman artist Neal Adams has long believed that Chicago has been the basis for Gotham, stating 'one of the things about Chicago is Chicago has alleys (which are virtually nonexistent in New York). Back alleys, that's where Batman fights all the bad guys.'. This has been an important part of mentioned culture";
+        case 'setting':
+            newTitle = 'SETTING';
+            newDescription = "Batman writer and editor Dennis O'Neil has said that, figuratively, Batman's Gotham City is akin to 'Manhattan below Fourteenth Street at eleven minutes past midnight on the coldest night in November.' Batman artist Neal Adams has long believed that Chicago has been the basis for Gotham, stating 'one of the things about Chicago is Chicago has alleys (which are virtually nonexistent in New York). Back alleys, that's where Batman fights all the bad guys.'. This has been an important part of Gotham's setting";
             break;
         case 'residents':
-            newTitle = 'Notable Residents';
+            newTitle = 'NOTABLE RESIDENTS';
             newDescription = "Bruce Wayne's place of residence is Wayne Manor, which is located on the outskirts of the city. His butler, Alfred Pennyworth, aids Bruce in his crusade to fight crime in Gotham. Over the years, in various Bat titles in the chronological DC Comics continuity, the caped crusader enlists the help of numerous characters, the first being his trusty sidekick, Robin. Although a singular title, many have donned the mantle of the Boy Wonder over the years. The first being Nightwing, then came Red Hood, Red Robin, and finally Batman's son Damian Wayne. In addition to the Robins or former Robins, there is also Catwoman, Batgirl, and Huntress.";
             break;
         default:
             newTitle = 'GOTHAM';
-            newDescription = "Gotham is a fictional city appearing in comic books published by DC Comics, best known as the home of Batman. The city was identified as Batman's place of residence in Batman #4 (December 1940) and has since been the primary setting for stories featuring the character. Gotham is traditionally depicted as being located in the U.S. state of New Jersey. Gotham's look and atmosphere was primarily influenced by New York City and Chicago,although it was designed to more generally resemble any major American city. Strega, the being's servant, says that the 'dark and often cursed character' of the city was influenced by the being who now uses the name 'Doctor Gotham.'";
+            newDescription = "Gotham is a fictional city appearing in comic books published by DC Comics, best known as the home of Batman. The city was identified as Batman's place of residence in Batman #4 (December 1940) and has since been the primary setting for stories featuring the character. Gotham is traditionally depicted as being located in the U.S. state of New Jersey. Gotham's look and atmosphere was primarily influenced by New York City and Chicago, although it was designed to more generally resemble any major American city. Strega, the being's servant, says that the 'dark and often cursed character' of the city was influenced by the being who now uses the name 'Doctor Gotham.'";
             break;
     }
     var titleToChange = $('.title');
@@ -113,8 +113,7 @@ $(".eachCard").click(function(){
     descriptionToChange.fadeOut(function(){
         descriptionToChange.text(newDescription);
         descriptionToChange.fadeIn();
-    });
-    
+    });    
     
 });
 
